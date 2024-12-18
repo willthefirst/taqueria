184d68c2be1e69c883548d7fe9e51476dada63a0
Will Moritz <willthefirst@gmail.com>
2024-12-17 23:29:36 -0800

    Noice got it.

--- guacamole/models.py
+++ guacamole/models.py
@@ -1,11 +1,4 @@
 from django.db import models
-
-class Member(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  phone = models.IntegerField(null=True)
-  joined_date = models.DateField()
-
 class TimestampedModel(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
@@ -14,13 +7,13 @@ class TimestampedModel(models.Model):
     abstract = True
 
 class AgeGroup(models.TextChoices):
-    CHILDREN_0_5 = '0-5', 'Children (0 to 5)'
-    CHILDREN_6_10 = '6-10', 'Children (6 to 10)'
-    PRETEENS_11_13 = '11-13', 'Preteens (11 to 13)'
-    ADOLESCENTS_14_18 = '14-18', 'Adolescents (14 to 18)'
-    YOUNG_ADULTS_19_24 = '19-24', 'Young Adults (19 to 24)'
-    ADULTS_25_64 = '25-64', 'Adults (25 to 64)'
-    OLDER_ADULTS_65_PLUS = '65+', 'Older Adults (65 +)'
+  CHILDREN_0_5 = '0-5', 'Children (0 to 5)'
+  CHILDREN_6_10 = '6-10', 'Children (6 to 10)'
+  PRETEENS_11_13 = '11-13', 'Preteens (11 to 13)'
+  ADOLESCENTS_14_18 = '14-18', 'Adolescents (14 to 18)'
+  YOUNG_ADULTS_19_24 = '19-24', 'Young Adults (19 to 24)'
+  ADULTS_25_64 = '25-64', 'Adults (25 to 64)'
+  OLDER_ADULTS_65_PLUS = '65+', 'Older Adults (65 +)'
   
 class Post(TimestampedModel):
   state = models.CharField(max_length=2)
