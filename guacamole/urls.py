059d74ce83a92af6be112bfb09b0e94eb0d7d0a0
Will Moritz <willthefirst@gmail.com>
2024-12-18 21:13:04 -0800

    Name

--- guacamole/urls.py
+++ guacamole/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('posts/', views.get_posts, name='TODO_YABADABA_REPLACE_ME'),
+  path('posts/', views.get_posts, name='posts_list'),
 ]
 
