a0391a33b0e793fad7939f4aa2585a246cb6a1f7
Will Moritz <willthefirst@gmail.com>
2024-12-29 13:57:56 -0800

    No superuser permisions needed

--- guacamole/signals.py
+++ guacamole/signals.py
@@ -7,6 +7,9 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def add_user_to_group(sender, instance, created, **kwargs):
+    if instance.is_superuser:
+        return
+        
     if created:
         group, created = Group.objects.get_or_create(name='Posts')
         instance.groups.add(group)
